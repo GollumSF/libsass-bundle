c2ad5fa286569671cd16753b2858f5929f516b92
Damien Duboeuf <smeagolworms4@gmail.com>
2015-07-28 11:26:12 +0200

    Configuration fil

--- libsass-bundle/GollumSF/LibSassBundle/DistributionBundle/Composer/ScriptHandler.php
+++ libsass-bundle/GollumSF/LibSassBundle/DistributionBundle/Composer/ScriptHandler.php
@@ -1,36 +1,40 @@
 <?php
 
 namespace GollumSF\LibSassBundle\DistributionBundle\Composer;
+use Symfony\Component\Process\Process;
+use Composer\Script\CommandEvent;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ScriptHandler {
 	
-	public static function submoduleInstall(CommandEvent $event) {
-		try {
-			
-			$vendorDir = $options['symfony-vendor-dir'];
-			echo shell_exec("git -C $vendorDir/gollumsf/libsass submodule update --init");
-			
-		} catch (\Exception $e) {
-			echo $e->getTraceAsString();
-			throw $e;
-		}
-	}
-	
 	public static function submoduleUpdate(CommandEvent $event) {
-		try {
-			
-			$vendorDir = $options['symfony-vendor-dir'];
-			echo shell_exec("git -C $vendorDir/gollumsf/libsass submodule update");
-			
-			$oldDir = realpath(getcwd());
-			chdir("$vendorDir/gollumsf/libsass/node-sass");
-	// 		echo shell_exec("npm install");
-			chdir($oldDir);
-			
-		} catch (\Exception $e) {
-			echo $e->getTraceAsString();
-			throw $e;
-		}
+		
+		self::executeCommand("git -C vendor/gollumsf/libsass submodule update --init");
+		
 	}
 	
+	public static function submoduleInstall(CommandEvent $event) {
+		
+		self::executeCommand("git -C vendor/gollumsf/libsass submodule foreach git reset --hard");
+		
+		$oldDir = realpath(getcwd());
+		chdir("vendor/gollumsf/libsass/node-sass");
+		if (file_exists("node_modules")) {
+			echo "Remove dir node_modules\n";
+			(new Filesystem())->remove("node_modules");
+		}
+		self::executeCommand("npm install");
+		chdir($oldDir);
+			
+	}
+	
+	protected static function executeCommand($cmd, $timeout = 300) {
+		
+		echo "Run => ".$cmd.":\n";
+		$process = new Process($cmd, null, null, null, $timeout);
+		$process->run(function ($type, $buffer) { echo $buffer; });
+		if (!$process->isSuccessful()) {
+			throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+		}
+	}
 }

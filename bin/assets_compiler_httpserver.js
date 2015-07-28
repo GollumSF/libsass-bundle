039d810196d27c37e7f40774d046fb53d070df18
Damien Duboeuf <smeagolworms4@gmail.com>
2015-07-28 18:29:13 +0200

    Pasth url

--- bin/assets_compiler_httpserver.js
+++ bin/assets_compiler_httpserver.js
@@ -9,9 +9,9 @@ var includePaths = [
 	rootPath+"/src/Glowbl/CommonBundle/Resources/public/css/"
 ];
 
-var ___COMPASS_HTTP_PATH___  = "//www.devglowbl.com";
-var ___COMPASS_FONT_DIR___   = "images";
-var ___COMPASS_IMAGES_DIR___ = "fonts";
+var ___COMPASS_HTTP_PATH___  = "//www.devglowbl.com/web";
+var ___COMPASS_FONT_DIR___   = "fonts";
+var ___COMPASS_IMAGES_DIR___ = "images";
 
 var http = require('http');
 
@@ -27,14 +27,29 @@ http.createServer(function (req, res) {
 		console.error ("404 File not found: "+assetPath);
 		return;
 	}
-
-	includePaths.push(bundlePath+"/Resources/compass/compass-mixins/lib/");
-	includePaths.push(bundlePath+"/Resources/compass/include/");
 	
-	var options =  ({
+	if (assetPath.substr(-3) == '.js') {
+		
+		fs.readFile(assetPath, "utf8", function (err,data) {
+			res.writeHead(200, {"Content-Type": "text/javascript"});
+			res.write(data);
+			res.end();
+		});
+		return
+	}
+	
+	var includeFinal = [
+		bundlePath+"/Resources/compass/include/",
+		bundlePath+"/Resources/compass/compass-mixins/lib/"
+	];
+	for (var i = 0; i < includePaths.length; i++) {
+		includeFinal.push(includePaths[i]);
+	}
+	
+	var options = ({
 		file: assetPath,
 		outputStyle: outputStyle,
-		includePaths: includePaths
+		includePaths: includeFinal
 	});
 	
 	console.log ("Compile: "+assetPath);
@@ -47,9 +62,9 @@ http.createServer(function (req, res) {
 			res.writeHead(200, {'Content-Type': 'text/css'});
 			
 			content = result.css.toString();
-			content = content.replace(new RegExp('___COMPASS_HTTP_PATH___', 'g') , ___COMPASS_HTTP_PATH___);
-			content = content.replace(new RegExp('___COMPASS_FONT_DIR___', 'g')  , ___COMPASS_FONT_DIR___);
-			content = content.replace(new RegExp('___COMPASS_IMAGES_DIR___', 'g'), ___COMPASS_IMAGES_DIR___);
+			content = content.replace(new RegExp('___COMPASS_HTTP_PATH___', 'g') , ___COMPASS_HTTP_PATH___  ? ___COMPASS_HTTP_PATH___ +'/' : '');
+			content = content.replace(new RegExp('___COMPASS_FONT_DIR___', 'g')  , ___COMPASS_FONT_DIR___   ? ___COMPASS_FONT_DIR___  +'/' : '');
+			content = content.replace(new RegExp('___COMPASS_IMAGES_DIR___', 'g'), ___COMPASS_IMAGES_DIR___ ? ___COMPASS_IMAGES_DIR___+'/' : '');
 			
 			res.write (content);
 		}

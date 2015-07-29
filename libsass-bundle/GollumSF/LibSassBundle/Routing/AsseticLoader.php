ff633249bdd63b128209942aec7540d3ad3d82d6
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-07-29 04:02:19 +0200

    Add nodejs compilation server for devmode (very fast)

--- libsass-bundle/GollumSF/LibSassBundle/Routing/AsseticLoader.php
+++ libsass-bundle/GollumSF/LibSassBundle/Routing/AsseticLoader.php
@@ -15,61 +15,29 @@ class AsseticLoader extends AsseticLoaderBase {
 	public function load($routingResource, $type = null)
 	{
 		$routes = parent::load($routingResource, $type);
-	
-// 		// routes
-// 		foreach ($this->am->getNames() as $name) {
-// 			$asset = $this->am->get($name);
-// 			$formula = $this->am->getFormula($name);
-	
-// 			$this->loadRouteForAsset($routes, $asset, $name);
-	
-// 			$debug = isset($formula[2]['debug']) ? $formula[2]['debug'] : $this->am->isDebug();
-// 			$combine = isset($formula[2]['combine']) ? $formula[2]['combine'] : !$debug;
-	
-// 			// add a route for each "leaf" in debug mode
-// 			if (!$combine) {
-// 				$i = 0;
-// 				foreach ($asset as $leaf) {
-// 					$this->loadRouteForAsset($routes, $leaf, $name, $i++);
-// 				}
-// 			}
-// 		}
-
-        var_dump($routes);exit();
+			
+		foreach ($this->am->getNames() as $name) {
+			$asset = $this->am->get($name);
+			$formula = $this->am->getFormula($name);
+			
+			$debug = isset($formula[2]['debug']) ? $formula[2]['debug'] : $this->am->isDebug();
+			$combine = isset($formula[2]['combine']) ? $formula[2]['combine'] : !$debug;
+			
+			if (!$combine && $debug) {
+				$i = 0;
+				foreach ($asset as $leaf) {
+					
+					$routeName = "_assetic_".$name."_".$i++;
+					$route = $routes->get($routeName);
+					if ($route) {
+						$route->setHost("dev.glowbl.com:8989");
+						$route->setPath($leaf->getSourceRoot()."/".$leaf->getSourcePath());
+					}
+				}
+			}
+		}
+		
 		return $routes;
 	}
 	
-	/**
-	 * Loads a route to serve an supplied asset.
-	 *
-	 * The fake front controller that {@link UseControllerWorker} adds to the
-	 * target URL will be removed before set as a route pattern.
-	 *
-	 * @param RouteCollection $routes The route collection
-	 * @param AssetInterface  $asset  The asset
-	 * @param string          $name   The name to use
-	 * @param integer         $pos    The leaf index
-	 */
-	private function loadRouteForAsset(RouteCollection $routes, AssetInterface $asset, $name, $pos = null)
-	{
-		$defaults = array(
-			'_controller' => 'assetic.controller:render',
-			'name'        => $name,
-			'pos'         => $pos,
-		);
-	
-		// remove the fake front controller
-		$pattern = str_replace('_controller/', '', $asset->getTargetPath());
-	
-		if ($format = pathinfo($pattern, PATHINFO_EXTENSION)) {
-			$defaults['_format'] = $format;
-		}
-	
-		$route = '_assetic_'.$name;
-		if (null !== $pos) {
-			$route .= '_'.$pos;
-		}
-	
-		$routes->add($route, new Route($pattern, $defaults));
-	}
 }

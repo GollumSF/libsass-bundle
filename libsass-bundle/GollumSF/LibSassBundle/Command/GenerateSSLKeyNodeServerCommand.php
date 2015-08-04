56469d73150f37200b422bfb312f94d86283ec7e
Damien Duboeuf <smeagolworms4@gmail.com>
2015-08-04 10:51:47 +0200

    Fix generate key

--- libsass-bundle/GollumSF/LibSassBundle/Command/GenerateSSLKeyNodeServerCommand.php
+++ libsass-bundle/GollumSF/LibSassBundle/Command/GenerateSSLKeyNodeServerCommand.php
@@ -49,6 +49,12 @@ class GenerateSSLKeyNodeServerCommand extends ContainerAwareCommand {
 		$process = new Process($cmd2, null, null, null, 0);
 		$process->run(function ($type, $buffer) { echo $buffer; });
 		if (!$process->isSuccessful()) {
+			throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd2)));
+		}
+		
+		$process = new Process($cmd3, null, null, null, 0);
+		$process->run(function ($type, $buffer) { echo $buffer; });
+		if (!$process->isSuccessful()) {
 			throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd3)));
 		}
 
@@ -58,45 +64,6 @@ class GenerateSSLKeyNodeServerCommand extends ContainerAwareCommand {
 		if (!$process->isSuccessful()) {
 			throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd4)));
 		}
-		
-// 		/* @var $kernel KernelInterface */
-// 		$kernel       = $this->getContainer ()->get ("kernel");
-// 		$node         = $this->getContainer ()->getParameter ("assetic.node.bin");
-// 		$port         = $this->getContainer ()->getParameter ("assetic.nodesass.compiler.port");
-// 		$https        = $this->getContainer ()->getParameter ("assetic.nodesass.compiler.https");
-// 		$portHttp     = $this->getContainer ()->getParameter ("assetic.nodesass.compiler.portHttps");
-// 		$node         = $this->getContainer ()->getParameter ("assetic.node.bin");
-// 		$outputStyle  = $this->getContainer ()->getParameter ("assetic.filter.nodesass.style");
-// 		$http_path    = $this->getContainer ()->getParameter ("assetic.filter.nodesass.http_path");
-// 		$fonts_dir    = $this->getContainer ()->getParameter ("assetic.filter.nodesass.fonts_dir");
-// 		$images_dir   = $this->getContainer ()->getParameter ("assetic.filter.nodesass.images_dir");
-// 		$bundlePath   = realpath($kernel->getBundle("GollumSFLibSassBundle")->getPath());
-// 		$rootPath     = realpath($kernel->getRootDir());
-// 		$compile      = $bundlePath."/Resources/nodejs/assets_compiler_httpserver.js";
-// 		$nodeSassPath = realpath(dirname($this->getContainer ()->getParameter ("assetic.nodesass.bin"))."/..");
-		
-// 		$includePaths = "";
-// 		foreach ($this->getContainer ()->getParameter ("assetic.filter.nodesass.load_paths") as $includePath) {
-// 			$includePaths .= " --includePaths $includePath";
-// 		}
-		
-// 		$cmd = "$node $compile 
-// 			--port $port ".
-// 			($https ? "--https 1 " : "")."
-// 			--portHttps $portHttp 
-// 			--nodeSassPath $nodeSassPath 
-// 			--rootPath $rootPath 
-// 			--bundlePath $bundlePath ".
-// 			($outputStyle ? "--outputStyle $outputStyle" : "")." 
-// 			--http_path $http_path 
-// 			--fonts_dir $fonts_dir 
-// 			--images_dir $images_dir$includePaths"
-// 		;
-		
-// 		$cmd = str_replace (["\n", "\t"], " ", $cmd);
-		
-// 		
-		
 	}
 	
 }
